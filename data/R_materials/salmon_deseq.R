ef9316ec90b8c1dc6f5097af30d720f998ccbea0
ryandkuster <ryan.kuster@gmail.com>
2025-05-25 14:44:22 -0400

    added salmon results and wgcna script

--- data/R_materials/salmon_deseq.R
+++ data/R_materials/salmon_deseq.R
@@ -4,49 +4,70 @@ library(GenomicFeatures)
 library(pheatmap)
 library(DESeq2)
 
+
+BiocManager::install("DEGreport")
+library(DEGreport)
+
 # create a workding directory
-setwd("/Users/ryankuster/Documents/projects/teaching/2024_06_EPP_575_RNA/data/derived")
+setwd("/Users/ryankuster/Documents/github/EPP_575_RNA_25/data/salmon_output")
 
 # give the full path to the "salmon_results" (it should end in "salmon_results"
-dir <- "/Users/ryankuster/Documents/projects/teaching/2024_06_EPP_575_RNA/data/derived/salmon_results"
+salmon_dir <- "/Users/ryankuster/Documents/github/EPP_575_RNA_25/data/salmon_output"
 
 # load the gff3 file, then create a transcript database/dataframe for use with deseq
-txdb <- makeTxDbFromGFF("/Users/ryankuster/Documents/projects/teaching/2024_06_EPP_575_RNA/data/reference/genomic.gff")
+txdb <- makeTxDbFromGFF("/Users/ryankuster/Downloads/EPP_575_RNA_25/IGV/genomic_modified.gff")
 keytypes(txdb)
 k <- keys(txdb, keytype = "CDSNAME")
 str(k)
 
 txdf = AnnotationDbi::select(txdb, k, "GENEID", "CDSNAME")
 
-# load in the metadata
-samples <- read_csv("/Users/ryankuster/Documents/projects/teaching/2024_06_EPP_575_RNA/data/derived/salmon_results/rename_samples_file.csv")
-
-Qfiles <- file.path(dir, samples$quant_file)
+samples <- read_csv("/Users/ryankuster/Downloads/EPP_575_RNA_25/DESEQ/salmon_output/salmon_data.csv")
+Qfiles <- file.path(salmon_dir, samples$quant_file)
 
 # this step imports the count data from salmon
-txi <- tximport(files = Qfiles, type = "salmon", tx2gene = txdf)
-colnames(txi$counts) <- samples$sample_id
-names(txi)
+txi <- tximport(files = Qfiles, type = "salmon", txOut = TRUE)
 head(txi$counts)
-summary(txi)
+colnames(txi$counts) <- samples$sample_id
+
+# convert fields to factors
+samples$treatment = factor(samples$treatment)
+samples$time = factor(samples$time)
+samples$replicate = factor(samples$replicate)
 
 # now we convert the txi object into a deseq-formatted object
-dds_data <- DESeqDataSetFromTximport(txi = txi, colData = samples, design = ~condition)
-dds <- DESeq(dds_data)
+dds <- DESeqDataSetFromTximport(txi = txi, colData = samples, design = ~ treatment + time + treatment:time)
+dds <- DESeq(dds, test="LRT", reduced = ~ treatment + time)
+dds <- dds[which(mcols(dds)$fullBetaConv),]
+res_LRT <- results(dds)
 
 # plot dispersion
 plotDispEsts(dds)
+vsd <- vst(dds)
+plotPCA(vsd, intgroup = c("time"))
+plotPCA(vsd, intgroup = c("replicate"))
+plotPCA(vsd, intgroup = c("treatment"))
 
+################################################################################
 # summarize results
 res <- results(dds)
 head(res)
+summary(res)
 
 # create a contrast with lfcThreshold and alpha cutoff (first list item is condition from samples object)
-res_sig <- results(dds, alpha = 0.05, contrast = c("condition", "Col_0h", "Col_3h"))
+# here are contrasts we can do:
 
-max_res_sig <- as.data.frame(res_sig[ which(res_sig$padj < 0.05 & abs(res_sig$log2FoldChange) > 2),])
-write.table(max_res_sig, file=paste0(dir,"/salmon_sig_results.csv"), row.names = T)
-summary(res_sig)
-plotMA(res_sig, ylim=c(-12,12))
+res_treatment <- results(dds, alpha = 0.05, contrast = c("treatment", "Col", "minusEGTA"))
+res_time <- results(dds, alpha = 0.05, contrast = c("time", "3h", "0h"))
+
+plotMA(res_treatment, ylim=c(-12,12))
+plotMA(res_time, ylim=c(-12,12))
+
+res_sig <- as.data.frame(res_treatment[ which(res_treatment$padj < 0.05),])
+res_sig <- res_sig[order(res_sig$padj, -abs(res_sig$log2FoldChange)),]
+
+write.csv(res_sig, file="salmon_featurecounts_sig_results.csv")
+
+
 
 
